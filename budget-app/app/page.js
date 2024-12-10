f79ad92e7869d4932cffdb7f1ceddf6014e9b8d7
Chelsea <chelsea.yang8@gmail.com>
2024-12-10 16:25:37 -0700

    start web app

---
+++ budget-app/app/page.js
@@ -0,0 +1,10 @@
+import LoginPage from "./login/login";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="mt-10 text-center text-5xl font-bold">Welcome to Our Budget App</h1>
+      <LoginPage />
+    </div>
+  );
+}
